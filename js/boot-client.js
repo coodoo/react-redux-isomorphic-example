44e376b4fe04827d3529ac8ed6a0e1046aebe391
Jeremy Lu <example@example.com>
2015-09-01 15:16:38 +0800

    chore(jscs): format files

--- js/boot-client.js
+++ js/boot-client.js
@@ -10,31 +10,33 @@ import ProductsContainer from './components/ProductsContainer';
 import ProductDetail from './components/ProductDetail';
 import {ProductState, ProductRecord, CartState, convertMapToImmutable} from './constants/Types';
 import {Router, Route} from 'react-router';
-import {history} from "react-router/lib/BrowserHistory";
+import {history} from 'react-router/lib/BrowserHistory';
 import { devTools, persistState } from 'redux-devtools';
 
 // 是否開啟 redux_devtool 面板
 window.$REDUX_DEVTOOL = false;
+
 // 資料是否已由 server 生成過，並在 client 還原了，如此可避免 client code 再撈一次資料
 window.$RESTORED = false;
 
 // 客戶端嚐試還原 state，如果有找到這個 elem 並且有內容，就代表為 isomorphic 版本
 let state = null;
-if( window.$REDUX_STATE ){
+if ( window.$REDUX_STATE ) {
 
 	// 解開 server 預先傳來的資料包，稍後會放入 store 成為 initState
 	state = window.$REDUX_STATE;
 
 	// begin marshalling data into Immutable types
-	state.products = new ProductState({
+	state.products = new ProductState( {
 		$fetched: document.location.pathname == '/',
-		productsById: convertMapToImmutable(state.products.productsById, ProductRecord),
+		productsById: convertMapToImmutable( state.products.productsById, ProductRecord ),
 		total: state.products.total,
 		currentProductId: state.products.currentProductId,
-	})
-	state.carts = new CartState({
-		cartsById: Immutable.List.of(...state.carts.cartsById)
-	})
+	} );
+
+	state.carts = new CartState( {
+		cartsById: Immutable.List.of( ...state.carts.cartsById ),
+	} );
 
 	// 用完就刪掉
 	delete window.$REDUX_STATE;
@@ -44,26 +46,25 @@ if( window.$REDUX_STATE ){
 	// console.log( 'state restored: ', state.products.toJS(), state.carts.toJS() );
 }
 
-
 // 就是 composeStores(), 將所有 stores 合併起來成為一個 composition(state, action) 指令
 // 將來操作它就等於操作所有 reducers
-const composedReducers = combineReducers(reducers);
+const composedReducers = combineReducers( reducers );
 
 // 掛上 reudx-devtools
 let cs;
-if( window.$REDUX_DEVTOOL ){
+if ( window.$REDUX_DEVTOOL ) {
 	cs = compose( devTools(), createStore );
-}else{
+}else {
 	cs = createStore;
 }
 
-const finalCreateStore = applyMiddleware( promiseMiddleware )(cs);
+const finalCreateStore = applyMiddleware( promiseMiddleware )( cs );
 
 // 重要：需視是否為 server rendering 而決定是否傳入 state 物件
 let store = state ? finalCreateStore( composedReducers, state ) : finalCreateStore( composedReducers );
 
 // 啟動 router，偷傳 store 進去方便它內部在每條 routing rule 啟動前先撈資料
-const routes = require("./routes/routing")(store);
+const routes = require( './routes/routing' )( store );
 
 ReactDOM.render(
 
@@ -71,5 +72,5 @@ ReactDOM.render(
 		<Router history={history} children={routes} />
 	</Provider>,
 
-	document.querySelector('.container')
+	document.querySelector( '.container' )
 );
