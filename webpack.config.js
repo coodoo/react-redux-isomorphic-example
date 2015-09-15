c86df162916df4e6d31ef84ebd9b7d1f055c367b
Jeremy Lu <example@example.com>
2015-09-15 10:55:40 +0800

    f

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',  // source-map, eval
+  devtool: 'eval-source-map',
   entry: [
     './js/boot-client' // client app 的進入點
   ],
