d77b8be8bbe52138eda3d1abea690dabc9fa4a21
Jeremy Lu <jeremy@pubulous.com>
2016-03-08 16:53:27 +0800

    fix(app): fix AsyncProps issues

--- common/components/TodoApp.jsx
+++ common/components/TodoApp.jsx
@@ -13,39 +13,21 @@ export default class TodoApp extends Component {
 
 	render() {
 
-		// console.log( 'this.props= ', this.props )
-		// isTransitioning is gone in react-router 1.0.0-rc1, pending request to put it back
-		const { isTransitioning } = this.props || false ;
-
 		// toggle redux-devPanel
-		var tool;
-		/*if ( 'undefined' !== typeof window && window.$REDUX_DEVTOOL == true ) {
-			tool = <DebugPanel top right bottom>
-					<DevTools store={this.context.store} monitor={LogMonitor} />
-					 </DebugPanel>
-		}*/
+		let tool;
+		// if ( 'undefined' !== typeof window && window.$REDUX_DEVTOOL == true ) {
+		// 	tool = <DebugPanel top right bottom>
+		// 			<DevTools store={this.context.store} monitor={LogMonitor} />
+		// 			 </DebugPanel>
+		// }
 
-		let nodes;
-
-		if ( isTransitioning ) {
-
-			nodes = (
-				<div>
-					{<div>LOADING...</div>}
-					{tool}
-				</div>
-			)
-
-		}else {
-
-			nodes = (
-				<div>
-					{this.props.main}
-					{this.props.cart}
-					{tool}
-				</div>
-			)
-		}
+		let nodes = (
+			<div>
+				{this.props.main}
+				{this.props.cart}
+				{tool}
+			</div>
+		)
 
 		return nodes;
 	}
